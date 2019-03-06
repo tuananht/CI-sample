9f9f6102a24f669d21e19618dcd527c68105ae79
Tuan Anh Tran (1765) <tuananhtran@kms.com.vn>
2019-03-06 09:30:38 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Samples for Docker images at https://github.com/katalon-studio/docker-images.
+
+# Katalon Studio
+
+    cd <project_root_directory>
+    ./katalon/run_chrome.sh
+    ./katalon/run_firefox.sh
+    
+If you are running the sample in Windows, also see issue #2.
---
+++ run_chrome.bat
@@ -0,0 +1,9 @@
+-- Tested against Docker Toolbox.
+-- Make sure the corresponding Windows drives/directories has been added to VirtualBox's shared directories https://docs.docker.com/toolbox/toolbox_install_windows/#optional-add-shared-directories
+
+@echo on
+
+set currentdir=%cd::\=/%
+set currentdir=%currentdir:\=/%
+
+docker run -t --rm -v "/%currentdir%:/tmp/source" -w /tmp/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
